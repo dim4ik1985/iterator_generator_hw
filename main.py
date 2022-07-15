f466135128c11402623c54570dc739947f8f3d8b
dim4ik85 <dim4ik1985@yandex.ru>
2022-07-15 12:21:42 +0300

    fix

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ if __name__ == '__main__':
     print('______________1___________________')
     for item in MyIterList(nested_list):
         print(item)
+
     # Комперхеншн итератора MyIterList
     flat_list = [item for item in MyIterList(nested_list)]
     print(flat_list)
@@ -33,7 +34,9 @@ if __name__ == '__main__':
     # 2
     for item in flat_generator(nested_list):
         print(item)
+
     print('__________________4*_________________')
+
     for item in mega_flat_generator(nested_list_max):
         print(item)
 
